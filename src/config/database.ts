6c40bbb066548d864c65e6ead2ebe3e93cac4904
Samueelx <gsammygitau@gmail.com>
2024-07-08 10:58:35 +0300

    prepping for delpoyment.

--- src/config/database.ts
+++ src/config/database.ts
@@ -5,6 +5,7 @@ import { JournalEntry } from '../models/journalEntryModel';
 
 const sequelize = new Sequelize({
     host: process.env.DB_URL,
+    port: Number(process.env.DB_PORT) || 5432,
     database: process.env.DB_NAME,
     dialect: 'postgres',
     username: process.env.DB_USERNAME,
